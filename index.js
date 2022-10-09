0cb65a1abe00fa84508a6be7a661fabfa54c64ce
GeordanD <geordan_daugherty@hotmail.com>
2022-10-09 16:16:08 -0500

    starting

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 let ages = [3, 9, 23, 64, 2, 8, 28, 93]
-function ages(num1, num2){
-    let num1 = ages[0];
-    let num2 = ages[7];
-    return num1 - num2;
+function subAges(){
+if (ages.length([]));
+return ages;
+
 }console.log(ages);
