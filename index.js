1c7a78aa352436f56481529a76444bf788cab09f
GeordanD <geordan_daugherty@hotmail.com>
2022-10-09 17:21:44 -0500

    subtraction added

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-let ages = [3, 9, 23, 64, 2, 8, 28, 93]
-function subAges(){
-if (ages.length([]));
-return ages;
+let ages = [3, 9, 23, 64, 2, 8, 28, 93];
+ages.push(30);//adds new number to end of the array dynamicly changes subtraction 
+let lastIndex = ages[ages.length - 1]; //finds the last index in the array and sets it into variable.
+let firstIndex = ages[0];// sets first index in a variable.
+let subIndex = lastIndex - firstIndex;// subtracts last index in array from first index in array.
 
-}console.log(ages);
+console.log(subIndex);
